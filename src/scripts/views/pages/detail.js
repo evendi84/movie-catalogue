f95c2374dcb832bc4ef6b60dd75bfed6517c00e8
evendi84 <82144355+evendi84@users.noreply.github.com>
2022-07-17 21:12:02 +0700

    Add files via upload

---
+++ src/scripts/views/pages/detail.js
@@ -0,0 +1,12 @@
+const Detail = {
+    async render() {
+        return `<h2>Detail page<h2>`;
+    },
+
+    async afterRender() {
+        /* fungsi ini di panggil setelah render */
+    },
+
+};
+
+export default Detail;
---
+++ src/scripts/views/pages/now-playing.js
@@ -0,0 +1,12 @@
+const NowPlaying = {
+    async render() {
+        return `<h2>Now PLaying</h2>`;
+    },
+
+    async afterRender() {
+        /* fungsi ini dipanggil setelah render */
+    },
+
+};
+
+export default NowPlaying;
